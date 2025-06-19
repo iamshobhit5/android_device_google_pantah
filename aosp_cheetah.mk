454e1e7e03bb254b11836f9708637de3e0769568
Michael Bestas <mkbestas@gmail.com>
2025-06-19 05:15:52 +0300

    pantah: Drop deprecated com.android.vndk.current.on_vendor
    Change-Id: I9e5827ebf0aa0438d85f16db55273c7c7306c9e0

--- aosp_cheetah.mk
+++ aosp_cheetah.mk
@@ -27,12 +27,3 @@ PRODUCT_DEVICE := cheetah
 PRODUCT_MODEL := AOSP on Cheetah
 PRODUCT_BRAND := Android
 PRODUCT_MANUFACTURER := Google
-
-DEVICE_MANIFEST_FILE := \
-	device/google/pantah/manifest.xml
-
-# Keep the VNDK APEX in /system partition for REL branches as these branches are
-# expected to have stable API/ABI surfaces.
-ifneq (REL,$(PLATFORM_VERSION_CODENAME))
-  PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
-endif
